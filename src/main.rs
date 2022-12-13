528f94fa665f0523dcff3f90860972e30aad42d7
ThomasLirio <afrolirio@gmail.com>
2022-12-13 21:06:16 +0000

    done

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+
+
+fn main() {
+    const V1_BEGIN: &str = "On the ";
+    const V1_END: &str = " day of Christmas, my true love sent to me";
+
+    let ordinals = ["first",
+                                "second", 
+                                "third", 
+                                "fourth", 
+                                "fifth", 
+                                "sixth", 
+                                "seventh",
+                                "eighth", 
+                                "ninth", 
+                                "tenth", 
+                                "eleventh", 
+                                "twelfth"];
+
+
+    let gifted_verses = ["And a partridge in a pear tree",
+                                     "Two turtledoves", 
+                                     "Three French hens", 
+                                     "Four calling birds", 
+                                     "Five gold rings (five golden rings)", 
+                                     "Six geese a-laying", 
+                                     "Seven swans a-swimming",
+                                     "Eight maids a-milking", 
+                                     "Nine ladies dancing", 
+                                     "Ten lords a-leaping", 
+                                     "Eleven pipers piping", 
+                                     "Twelve drummers drumming"];
+
+    //let mut lyrics = String::from("Twelve Days Of Christmas\n\n\n");
+    println!("Twelve Days Of Christmas\n\n\n");
+
+    for i in 0..=11 {
+    
+        let cardinal = ordinals[i];
+        println!("{V1_BEGIN}{cardinal}{V1_END}");
+        if i == 0 {
+            println!("A partridge in a pear tree");
+        } else {
+            for j in (0..=i).rev() {
+                if j==10 && i==10 {
+                    println!("I sent {}", gifted_verses[j]);
+                } else {
+                    println!("{}", gifted_verses[j]);
+                }
+            }
+        }
+        println!("\n");
+    }
+   
+    println!("{}", gifted_verses[0]);
+}
